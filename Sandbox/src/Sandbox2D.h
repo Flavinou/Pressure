25307c64f5f57a68906915141fa5e20d16a92774
Flavinou <flafebvay@gmail.com>
2022-05-02 15:41:23 +0200

    Added transforms & textures to Renderer2D

--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -20,6 +20,7 @@ private:
 	// Temporary
 	Pressure::Ref<Pressure::VertexArray> m_SquareVA;
 	Pressure::Ref<Pressure::Shader> m_FlatColorShader;
+	Pressure::Ref<Pressure::Texture2D> m_VoronoiTexture;
 
 	glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
 };
