2665194c2480aed7830d37d94c58520f2827860c
Flavinou <flafebvay@gmail.com>
2022-05-13 17:49:55 +0200

    Renderer2D API minor improvements
    - Texture tiling
    - Texture tinting

--- Pressure/src/Platform/OpenGL/OpenGLShader.cpp
+++ Pressure/src/Platform/OpenGL/OpenGLShader.cpp
@@ -216,6 +216,13 @@ namespace Pressure
 		UploadUniformInt(name, value);
 	}
 
+	void OpenGLShader::SetFloat(const std::string& name, float value)
+	{
+		PRS_PROFILE_FUNCTION();
+
+		UploadUniformFloat(name, value);
+	}
+
 	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
 	{
 		PRS_PROFILE_FUNCTION();
