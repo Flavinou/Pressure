ccb97e01f46f5197182d801c9b6d1798363c3ebf
Flavinou <flafebvay@gmail.com>
2025-03-14 11:10:55 +0100

    Recalculate camera view projection matrix on zoom level assignment

--- Pressure/src/Pressure/Renderer/OrthographicCameraController.cpp
+++ Pressure/src/Pressure/Renderer/OrthographicCameraController.cpp
@@ -74,14 +74,19 @@ namespace Pressure
         dispatcher.Dispatch<WindowResizeEvent>(PRS_BIND_EVENT_FN(OrthographicCameraController::OnWindowResized));
     }
 
+    void OrthographicCameraController::CalculateView()
+    {
+        m_Bounds = { -m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel };
+        m_Camera.SetProjection(m_Bounds.Left, m_Bounds.Right, m_Bounds.Bottom, m_Bounds.Top);
+    }
+
     bool OrthographicCameraController::OnMouseScrolled(MouseScrolledEvent& e)
     {
         PRS_PROFILE_FUNCTION();
 
         m_ZoomLevel -= e.GetYOffset() * 0.25f;
         m_ZoomLevel = std::max(m_ZoomLevel, 0.25f);
-        m_Bounds = { -m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel };
-        m_Camera.SetProjection(m_Bounds.Left, m_Bounds.Right, m_Bounds.Bottom, m_Bounds.Top);
+        CalculateView();
         return false;
     }
 
@@ -90,8 +95,7 @@ namespace Pressure
         PRS_PROFILE_FUNCTION();
 
         m_AspectRatio = (float)e.GetWidth() / (float)e.GetHeight();
-        m_Bounds = { -m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel };
-        m_Camera.SetProjection(m_Bounds.Left, m_Bounds.Right, m_Bounds.Bottom, m_Bounds.Top);
+        CalculateView();
         return false;
     }
 
