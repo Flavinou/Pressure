70382e439c8d3f4610b38cff3d692ab21bfbf2c4
Flavinou <flafebvay@gmail.com>
2025-03-20 09:26:20 +0100

    Send logs to dedicated file

--- Pressure/src/Pressure/Core/Log.cpp
+++ Pressure/src/Pressure/Core/Log.cpp
@@ -2,6 +2,7 @@
 #include "Pressure/Core/Log.h"
 
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace Pressure
 {
@@ -10,12 +11,22 @@ namespace Pressure
 
 	void Log::Init()
 	{
-		spdlog::set_pattern("%^[%T] %n: %v%$");
-		s_CoreLogger = spdlog::stdout_color_mt("PRESSURE");
-		s_CoreLogger->set_level(spdlog::level::trace);
+        std::vector<spdlog::sink_ptr> logSinks;
+        logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+        logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Pressure.log", true));
 
-		s_ClientLogger = spdlog::stdout_color_mt("CLIENT");
+        logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+        logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+        s_CoreLogger = std::make_shared<spdlog::logger>("Pressure", begin(logSinks), end(logSinks));
+        spdlog::register_logger(s_CoreLogger);
+		s_CoreLogger->set_level(spdlog::level::trace);
+		s_CoreLogger->flush_on(spdlog::level::trace);
+
+        s_ClientLogger = std::make_shared<spdlog::logger>("CLIENT", begin(logSinks), end(logSinks));
+        spdlog::register_logger(s_ClientLogger);
 		s_ClientLogger->set_level(spdlog::level::trace);
+        s_CoreLogger->flush_on(spdlog::level::trace);
 	}
 
 }
