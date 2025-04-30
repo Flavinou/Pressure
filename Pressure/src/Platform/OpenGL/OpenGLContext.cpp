375ec786ee0fd3931cfe4670122bf2617f0566e9
Flavinou <flafebvay@gmail.com>
2025-04-30 16:57:39 +0200

    Added OpenGL version check

--- Pressure/src/Platform/OpenGL/OpenGLContext.cpp
+++ Pressure/src/Platform/OpenGL/OpenGLContext.cpp
@@ -25,6 +25,8 @@ namespace Pressure
         PRS_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
         PRS_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
         PRS_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
+
+		PRS_CORE_ASSERT(GLVersion.major > 4 || (GLVersion.major == 4 && GLVersion.minor >= 5), "Pressure requires at least OpenGL version 4.5!");
     }
 
     void OpenGLContext::SwapBuffers()
