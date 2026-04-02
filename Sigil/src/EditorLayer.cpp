8ba4024e3ddde2efe9a1837efb61e406cb912cb6
Flavien Febvay <flavien.febvay@ubisoft.com>
2026-04-02 16:41:13 +0200

    Added play/stop button to editor

--- Sigil/src/EditorLayer.cpp
+++ Sigil/src/EditorLayer.cpp
@@ -36,6 +36,9 @@ namespace Pressure
         fbSpec.Height = 720;
         m_FrameBuffer = FrameBuffer::Create(fbSpec);
 
+		m_IconPlay = Texture2D::Create("resources/icons/play_icon.png");
+		m_IconStop = Texture2D::Create("resources/icons/stop_icon.png");
+
         m_VoronoiTexture = Texture2D::Create("assets/textures/Voronoi2.png");
 
         m_ActiveScene = CreateRef<Scene>();
@@ -125,14 +128,6 @@ namespace Pressure
             m_ActiveScene->OnViewportResize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
         }
 
-        // Update
-        if (m_ViewportFocused)
-        {
-            m_CameraController.OnUpdate(ts);
-        }
-
-		m_EditorCamera.OnUpdate(ts);
-
         // Render
         Renderer2D::ResetStats();
 
@@ -144,8 +139,24 @@ namespace Pressure
 		// Clear our entity ID attachment to -1
 		m_FrameBuffer->ClearAttachment(1, -1);
 
-        // Update Scene
-        m_ActiveScene->OnUpdateEditor(ts, m_EditorCamera);
+		switch (m_SceneState)
+		{
+			case SceneState::Edit:
+			{
+				if (m_ViewportFocused)
+					m_CameraController.OnUpdate(ts);
+
+				m_EditorCamera.OnUpdate(ts);
+
+				m_ActiveScene->OnUpdateEditor(ts, m_EditorCamera);
+				break;
+			}
+			case SceneState::Play:
+			{
+				m_ActiveScene->OnUpdateRuntime(ts);
+				break;
+			}
+		}
 
 		auto [mx, my] = ImGui::GetMousePos();
 		mx -= m_ViewportBounds[0].x;
@@ -366,6 +377,8 @@ namespace Pressure
         ImGui::End();
         ImGui::PopStyleVar();
 
+		UI_Toolbar();
+
         ImGui::End();
     }
 
@@ -481,6 +494,46 @@ namespace Pressure
 			SceneSerializer serializer(m_ActiveScene);
 			serializer.Serialize(filePath);
 		}
+	}
+
+	void EditorLayer::OnScenePlay()
+	{
+    	m_SceneState = SceneState::Play;
+	}
+
+	void EditorLayer::OnSceneStop()
+	{
+		m_SceneState = SceneState::Edit;
+	}
+
+	void EditorLayer::UI_Toolbar()
+	{
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 2));
+		ImGui::PushStyleVar(ImGuiStyleVar_ItemInnerSpacing, ImVec2(0, 0));
+		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 0, 0, 0));
+		auto& colors = ImGui::GetStyle().Colors;
+		const auto& buttonHovered = colors[ImGuiCol_ButtonHovered];
+		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(buttonHovered.x, buttonHovered.y, buttonHovered.z, 0.5f));
+    	const auto& buttonActive = colors[ImGuiCol_ButtonActive];
+		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(buttonActive.x, buttonActive.y, buttonActive.z, 0.5f));
+
+		ImGui::Begin("##toolbar", nullptr, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
+
+		float size = ImGui::GetWindowHeight() - 4.0f;
+		Ref<Texture2D> icon = m_SceneState == SceneState::Edit ? m_IconPlay : m_IconStop;
+		ImGui::SetCursorPosX((ImGui::GetWindowContentRegionMax().x * 0.5f) - (size * 0.5f));
+		if (ImGui::ImageButton(reinterpret_cast<ImTextureID>(icon->GetRendererID()), ImVec2(size, size), ImVec2(0, 0), ImVec2(1, 1), 0))
+		{
+			if (m_SceneState == SceneState::Edit)
+				OnScenePlay();
+			else if (m_SceneState == SceneState::Play)
+				OnSceneStop();
+		}
+
+		ImGui::PopStyleVar(2);
+		ImGui::PopStyleColor(3);
+
+		ImGui::End();
 	}
 
 }
--- Sigil/src/EditorLayer.h
+++ Sigil/src/EditorLayer.h
@@ -31,6 +31,11 @@ namespace Pressure
 		void OpenScene(const std::filesystem::path& path);
 		void SaveSceneAs();
 
+		void OnScenePlay();
+		void OnSceneStop();
+
+		void UI_Toolbar();
+
 	private:
 		OrthographicCameraController m_CameraController;
 
@@ -60,9 +65,19 @@ namespace Pressure
 
 		int m_GizmoType = -1;
 
+		enum class SceneState
+		{
+			Edit = 0,
+			Play = 1,
+		};
+		SceneState m_SceneState = SceneState::Edit;
+
 		// Panels
 		SceneHierarchyPanel m_SceneHierarchyPanel{};
 		ContentBrowserPanel m_ContentBrowserPanel;
+		
+		// Editor resources
+		Ref<Texture2D> m_IconPlay, m_IconStop;
 	};
 
 }
