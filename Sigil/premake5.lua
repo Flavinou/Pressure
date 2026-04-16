56c546aa6472a4a6c23e76efebf840a15e05cb04
Flavien Febvay <flavien.febvay@ubisoft.com>
2026-04-16 15:36:31 +0200

    Rmoved local debug libs
    * Also removed old post-build commands

--- Sigil/premake5.lua
+++ Sigil/premake5.lua
@@ -41,11 +41,6 @@ project "Sigil"
         runtime "Debug"
         symbols "on"
 
-		postbuildcommands
-		{
-			"{COPYDIR} \"%{LibraryDir.VulkanSDK_DebugDLL}\" \"%{cfg.targetdir}\""
-		}
-
     filter "configurations:Release"
         defines "PRS_RELEASE"
         runtime "Release"
